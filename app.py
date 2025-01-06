51301530c02f0dc6d789f59b8b3ca6d6c19820a1
ehab <ehab.sadiq89@gmail.com>
2025-01-06 19:31:35 +0200

    Fix model path in app.py to use forward slashes for compatibility

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 
 # Load the pre-trained model
-model = load_model('Models\model.keras')
+model = load_model('Models/model.keras')
 
 # Define emotion labels
 emotion_labels = ["Angry", "Disgust", "Fear",
